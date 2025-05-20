3f49636cc7b71ddf0582bbbbea5a07842acee356
Oooooq <97772192+Oooooq@users.noreply.github.com>
2025-05-20 12:14:31 -0400

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const Corrosion = require("./lib/server")
 const proxy = new Corrosion({
     prefix: config.prefix,
     codec: config.codec,
-    title: "Shadow",
+    title: "YAZ",
     forceHttps: true,
     requestMiddleware: [
         Corrosion.middleware.blacklist([
@@ -53,5 +53,5 @@ app.use(function (req, res) {
 })
 
 app.listen(port, () => {
-    console.log(`Shadow is running at localhost:${port}`)
+    console.log(`YAZ is running at localhost:${port}`)
 })
